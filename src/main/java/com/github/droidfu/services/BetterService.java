6d9bb97a55579845b413fa303eccc645909ca10b
Matthias Kaeppler <matthias@qype.com>
2009-11-17 18:33:53 +0100

    port BetterService to use the new mechanism for context instance housekeeping

--- src/main/java/com/github/droidfu/services/BetterService.java
+++ src/main/java/com/github/droidfu/services/BetterService.java
@@ -1,29 +1,18 @@
 package com.github.droidfu.services;
 
-import java.lang.ref.WeakReference;
-
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import com.github.droidfu.DroidFuApplication;
+
 public class BetterService extends Service {
-
-    private static volatile WeakReference<BetterService> currentInstance;
-
-    public static synchronized BetterService getCurrentInstance() {
-        return currentInstance.get();
-    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        currentInstance = new WeakReference<BetterService>(this);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        currentInstance.clear();
+        ((DroidFuApplication) getApplication()).setActiveContext(getClass().getCanonicalName(),
+            this);
     }
 
     @Override
