39264cd7c116caa28578b3b039193c14b5a7b163
Matthias Kaeppler <m.kaeppler@gmail.com>
2009-12-23 16:18:20 +0100

    add licensing information to all source files and bundle license text with project

--- src/main/java/com/github/droidfu/http/HttpPost.java
+++ src/main/java/com/github/droidfu/http/HttpPost.java
@@ -1,3 +1,18 @@
+/* Copyright (c) 2009 Matthias Käppler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.droidfu.http;
 
 import java.util.HashMap;
@@ -7,13 +22,11 @@ import org.apache.http.HttpEntityEnclosingRequest;
 
 class HttpPost extends BetterHttpRequest {
 
-    HttpPost(String url, HttpEntity payload,
-            HashMap<String, String> defaultHeaders) {
+    HttpPost(String url, HttpEntity payload, HashMap<String, String> defaultHeaders) {
         this.request = new org.apache.http.client.methods.HttpPost(url);
         ((HttpEntityEnclosingRequest) request).setEntity(payload);
 
-        request.setHeader(HTTP_CONTENT_TYPE_HEADER,
-                payload.getContentType().getValue());
+        request.setHeader(HTTP_CONTENT_TYPE_HEADER, payload.getContentType().getValue());
         for (String header : defaultHeaders.keySet()) {
             request.setHeader(header, defaultHeaders.get(header));
         }
