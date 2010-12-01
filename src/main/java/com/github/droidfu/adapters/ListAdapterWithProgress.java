27a5c639928bcb9974d27e657ee9783325f3b350
Matthias Kaeppler <m.kaeppler@gmail.com>
2010-12-01 14:33:04 +0100

    remove debug output

--- src/main/java/com/github/droidfu/adapters/ListAdapterWithProgress.java
+++ src/main/java/com/github/droidfu/adapters/ListAdapterWithProgress.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009 Matthias Käppler
+/* Copyright (c) 2009 Matthias Kaeppler
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -147,8 +147,6 @@ public abstract class ListAdapterWithProgress<T> extends BaseAdapter {
     }
 
     public final View getView(int position, View convertView, ViewGroup parent) {
-        System.out.println("adapter Android convertView for pos " + position + " null = "
-                + (convertView == null));
         if (isPositionOfProgressElement(position)) {
             return progressView;
         }
